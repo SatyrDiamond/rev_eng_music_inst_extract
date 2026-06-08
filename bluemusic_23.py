f19b17585de9ddeb150bb0f0ba4303351fbd6f2e
SatyrDiamond <109001272+SatyrDiamond@users.noreply.github.com>
2026-06-08 14:17:32 -0400

    Update bluemusic_23.py

--- bluemusic_23.py
+++ bluemusic_23.py
@@ -5,7 +5,7 @@ def do_blue_23(filename):
 	revpol = samplereverpol('Blue (23).wav', 122)
 
 	inst_slaps = revpol.get_revpol_fixed(11, 15)
-	to_wav('blue_23_inst/inst_slaps.wav', inst_slaps, revpol)
+	to_wav('blue_23_inst/drums_clicks.wav', inst_slaps, revpol)
 
 	inst_wao = revpol.get_revpol_fixed(11, 27)
 	to_wav('blue_23_inst/inst_wao.wav', inst_wao, revpol)
@@ -17,7 +17,7 @@ def do_blue_23(filename):
 	comb_high_predrum = revpol.get_revpol_fixed(7, 8)
 	comb_high_predrum_revcrash = revpol.get_revpol_fixed(7, 9)
 	inst_revcrash = comb_high_predrum-comb_high_predrum_revcrash
-	to_wav('blue_23_inst/inst_revcrash.wav', inst_revcrash, revpol)
+	to_wav('blue_23_inst/drums_revcrash.wav', inst_revcrash, revpol)
 	
 	comb_high_revcrash = revpol.get_revpol_fixed(2, 14)
 	inst_high = -(comb_high_revcrash+inst_revcrash)
@@ -29,7 +29,7 @@ def do_blue_23(filename):
 
 	comb_hicrash_bass = revpol.get_revpol_fixed(4, 17)
 	inst_hicrash = (comb_hicrash_bass+inst_bass)
-	to_wav('blue_23_inst/inst_hicrash.wav', inst_hicrash, revpol)
+	to_wav('blue_23_inst/drums_hicrash.wav', inst_hicrash, revpol)
 
 	wip = revpol.get_fixedbar(29).copy()
 	val75 = int(len(wip)*0.75)
@@ -37,7 +37,7 @@ def do_blue_23(filename):
 	wip -= inst_bass
 	wip -= inst_slaps
 	inst_crash = wip[:val75]
-	to_wav('blue_23_inst/inst_crash.wav', inst_crash, revpol)
+	to_wav('blue_23_inst/drums_crash.wav', inst_crash, revpol)
 
 	wip = revpol.get_fixedbar(35).copy()
 	val75 = int(len(wip)*0.75)
@@ -47,7 +47,7 @@ def do_blue_23(filename):
 	wip[:val75] -= inst_slaps[:val75]
 	wip[:val75] -= inst_bass[:val75]
 	inst_predrum = wip[val75:]
-	to_wav('blue_23_inst/inst_predrum.wav', inst_predrum, revpol)
+	to_wav('blue_23_inst/drums_predrum.wav', inst_predrum, revpol)
 
 	wip = revpol.get_fixedbar(24).copy()
 	wip -= inst_high
@@ -60,7 +60,7 @@ def do_blue_23(filename):
 	wip -= comb_hiloop
 	wip -= inst_revcrash
 	inst_locrash = wip
-	to_wav('blue_23_inst/inst_locrash.wav', inst_locrash, revpol)
+	to_wav('blue_23_inst/drums_locrash.wav', inst_locrash, revpol)
 
 
 os.makedirs('blue_23_inst', exist_ok=True)
